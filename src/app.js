3968ad2eebf3d83958007dee96aafd31dfcdd6ec
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-10-21 20:29:20 +0000

    Finished Project

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  let randomNumberGenerator = Math.floor(Math.random() * 10);
+  document.querySelector("#excuse").innerHTML = makeAnExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let makeAnExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let indexWho = Math.floor(Math.random() * who.length);
+  let indexAction = Math.floor(Math.random() * action.length);
+  let indexWhat = Math.floor(Math.random() * what.length);
+  let indexWhen = Math.floor(Math.random() * when.length);
+
+  return (
+    who[indexWho] +
+    " " +
+    action[indexAction] +
+    " " +
+    what[indexWhat] +
+    " " +
+    when[indexWhen]
+  );
 };
